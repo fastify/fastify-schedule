33d76a6496b1477e3a96aa691f7fe7d517244811
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 12:52:50 +0000

    build(deps-dev): replace standard with neostandard (#148)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,18 +1,18 @@
-import { FastifyPluginCallback } from "fastify";
-import { ToadScheduler } from "toad-scheduler";
+import { FastifyPluginCallback } from 'fastify'
+import { ToadScheduler } from 'toad-scheduler'
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyInstance {
     scheduler: ToadScheduler;
   }
 }
 
-type FastifySchedule = FastifyPluginCallback;
+type FastifySchedule = FastifyPluginCallback
 
 declare namespace fastifySchedule {
   export const fastifySchedule: FastifySchedule
   export { fastifySchedule as default }
 }
 
-declare function fastifySchedule(...params: Parameters<FastifySchedule>): ReturnType<FastifySchedule>
+declare function fastifySchedule (...params: Parameters<FastifySchedule>): ReturnType<FastifySchedule>
 export = fastifySchedule
